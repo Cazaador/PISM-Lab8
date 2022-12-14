55d719f189f53bafe8743d811dc9da68769add32
Cazaador <artiom.zhuravliov79@gmail.com>
2022-12-14 21:17:45 +0300

    added function in da class

--- src/laba8/gr074001/zhuravliov/NewClass.java
+++ src/laba8/gr074001/zhuravliov/NewClass.java
@@ -3,5 +3,10 @@ package laba8.gr074001.zhuravliov;
 import java.util.Scanner;
 
 public class NewClass {
-
+    public static String function(String str){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение:");
+        str = scanner.nextLine();
+        return str;
+    }
 }
