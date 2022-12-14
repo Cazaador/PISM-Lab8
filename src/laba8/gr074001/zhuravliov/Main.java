2123839db0b9a64e6c2e38f748b857743b7f7e05
Cazaador <artiom.zhuravliov79@gmail.com>
2022-12-14 21:02:08 +0300

    first commit

---
+++ src/laba8/gr074001/zhuravliov/Main.java
@@ -0,0 +1,17 @@
+package laba8.gr074001.zhuravliov;
+
+import laba8.gr074001.zhuravliov.string_array.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StaticStringArray stringArray = new StaticStringArray(3);
+
+        stringArray.add("опаопа ");
+        stringArray.add("о папа");
+        stringArray.add("Привет");
+        stringArray.add("Андрей");
+        stringArray.add("ТУТУРУРУ");
+
+        stringArray.print();
+    }
+}
